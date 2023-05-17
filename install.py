b8dc7712be71370cb80965b820bfb95d2ea00838
jansen <liasece@gmail.com>
2023-05-17 04:06:05 +0000

    fix pip install file path

--- install.py
+++ install.py
@@ -12,4 +12,4 @@ with git.Repo(my_path) as repo:
 python_requirements_file = os.path.join(my_path, "requirements.txt")
 
 with open(python_requirements_file) as file:
-    launch.run_pip(f"install -r {python_requirements_file}", f"sd-webui-train-tools requirement: {python_requirements_file}")
+    launch.run_pip(f'install -r "{python_requirements_file}"', f"sd-webui-train-tools requirement: {python_requirements_file}")
